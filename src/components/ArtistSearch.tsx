5dd15bb1c8246502426437d92de8a1c1f5330e7d
Amelia Laundy <amelialaundy@gmail.com>
2018-08-17 09:17:19 +1200

    dynamically add artist input search boxes

--- src/components/ArtistSearch.tsx
+++ src/components/ArtistSearch.tsx
@@ -1,59 +1,84 @@
 // src/components/search.tsx
-
+// tslint:disable:no-console
 import {debounce} from 'lodash'
 import * as React from 'react';
 
 interface IState {
-    artistList: string[];
+		artistList: string[];
 }
 
 interface IProps {
-    visible: boolean;
-    onSearch: (artists: string[]) => void;
-    onError: (e: any) => void;
+		visible: boolean;
+		onSearch: (artists: string[]) => void;
+		onError: (e: any) => void;
 }
 
 class ArtistSearch extends React.Component<IProps, IState> {
-    public update = debounce((event: React.ChangeEvent<HTMLInputElement>) => { 
-        // tslint:disable-next-line:no-console
-        console.log('debounce', event.target)
-        const artistList = event.target.value.split(',').filter((i: string) => i);
-        this.setState({artistList})
-        this.props.onSearch(artistList);
-    }, 1000);
-    
-    constructor(props: IProps) {
-        super(props)
-        this.state = { artistList: []}
-    }
+		public update = debounce((event: React.ChangeEvent<HTMLInputElement>) => { 
+				const {artistList} = this.state;
+				
+				const newList= [...artistList, event.target.value];
+				console.log('debounce', event.target, artistList, newList)
+				
+				this.setState({artistList: newList});
+				this.props.onSearch(newList); 
+		}, 1000);
+		
+		constructor(props: IProps) {
+				super(props)
+				this.state = { artistList: []}
+		}
 
-    public updateInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.persist();
-        this.update(event);
-        // const artistList = event.target.value.split(',');
-        // this.setState({artistList})
-        // this.props.onSearch(artistList);
-    }
-    // public onSubmit = async (e: any) => {
-    //     e.preventDefault()
-    //     const artistList = this.state.artistList;
-    //     this.props.onSearch(artistList);
-    // }
+		public updateInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+				event.persist();
+				this.update(event);
 
-    // public mapArtistListForDisplay = () => {
-    //     return this.state.artistList.join(', ')
-    // }
+		}
 
-    public render() {
-    return (
-        <div className="type-container artist">
-            <label>
-            Enter up to 5 artist names seperated by a comma:
-            </label>
-            <input className={'artistSearch'} type="text" name="artistList"  onChange={this.updateInput} />
-        </div>
-    );
-    }
+		public addArtist = (event: React.ChangeEvent<HTMLInputElement>) => {
+				event.persist();
+				this.update(event);
+		}
+
+		public removeArtist = (event: React.ChangeEvent<HTMLInputElement>) => {
+				const {artistList} = this.state;
+				
+				const newList= artistList.filter(a => a !== event.target.value)
+				console.log('debounce', event.target, artistList, newList)
+				
+				this.setState({artistList: newList});
+				this.props.onSearch(newList); 
+		}
+
+		public artistRows = () => {
+				const currentNumArtists = this.state.artistList.length;// 1,2,3,4,5
+				const numbers = [1, 2, 3, 4, 5];
+				return numbers.map((x, i) => { // i = 0,1,2,3,4
+						const displayClassName = 'show';
+						const hideClassName = 'hide';
+						const showInput = i <= currentNumArtists;
+						const className = showInput ? displayClassName : hideClassName;
+
+						return (
+								<span key={i + 1} className={`artist ${className}`}>
+										<input className={'artistSearch'} type="text" name="artistList" onChange={this.addArtist} />
+										<button>remove</button>
+								</span>)
+				}
+				)
+		}
+
+		public render() {
+				return (
+						<div className="type-container artist">
+								<label>
+										Enter up to 5 artist names seperated by a comma:
+						</label>
+						{this.artistRows()}
+
+						</div>
+				);
+		}
 }
 
 export default ArtistSearch;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,143 +3,143 @@ import * as React from 'react';
 
 import '../App.css';
 
-import {IQueryAttribute} from '../Attributes'
-import {Spotify} from '../helpers/Spotify'
+import { IQueryAttribute } from '../Attributes'
+import { Spotify } from '../helpers/Spotify'
 import ArtistSearch from './ArtistSearch';
 import GenreSearch from './GenreSearch';
 import QueryAttributes from './QueryAttributes';
 
 export interface IState {
-  artistsIds: string[]; 
-  error?: any;
-  initiated: boolean;
-  token?: string;
-  recommendations?: SpotifyApi.RecommendationsFromSeedsResponse;
-  searchOptions: SpotifyApi.RecommendationsOptionsObject;
-  genres?: string[];
+	artistsIds: string[];
+	error?: any;
+	initiated: boolean;
+	token?: string;
+	recommendations?: SpotifyApi.RecommendationsFromSeedsResponse;
+	searchOptions: SpotifyApi.RecommendationsOptionsObject;
+	genres?: string[];
 }
 
 class Home extends React.Component<any, IState> {
-  private spotify: Spotify;
-  private loginUrl: string = "https://accounts.spotify.com/authorize?client_id=760467e647f4408dab802bd3f3d7a82e&redirect_uri=http:%2F%2F192.168.178.49:3000%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123";
+	private spotify: Spotify;
+	private loginUrl: string = "https://accounts.spotify.com/authorize?client_id=760467e647f4408dab802bd3f3d7a82e&redirect_uri=http:%2F%2F192.168.178.49:3000%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123";
 
-  constructor(props: any) {
-    super(props)
-    const tokenInStorage = localStorage.getItem('token');
-    if (tokenInStorage) { this.initSpotify(tokenInStorage) }
-    
-    this.state = {  artistsIds: [], error: {}, token: tokenInStorage ? tokenInStorage: '', initiated: tokenInStorage ? true : false, searchOptions: {}}
-  }
+	constructor(props: any) {
+		super(props)
+		const tokenInStorage = localStorage.getItem('token');
+		if (tokenInStorage) { this.initSpotify(tokenInStorage) }
 
-  public initSpotify = (token: string) => {
-    this.spotify = new Spotify(token);
-  }
+		this.state = { artistsIds: [], error: {}, token: tokenInStorage ? tokenInStorage : '', initiated: tokenInStorage ? true : false, searchOptions: {} }
+	}
 
-  public componentDidMount() {
-    if (this.state.initiated) {
-      this.getGenres()
-    }
-  }
+	public initSpotify = (token: string) => {
+		this.spotify = new Spotify(token);
+	}
 
-  public getArtistIds = async (artistNames: string[]) => {
-    // tslint:disable-next-line:no-console
-    console.log('getting artists', artistNames)
-    const artists = (await this.spotify.searchArtists(artistNames))
-    // tslint:disable-next-line:no-console
-    console.log('artists from spot', artists)
-    const artistsIds = artists.filter(a => !!a).map(r => r.id);
-    const opts = this.state.searchOptions;
-    opts.seed_artists = artistsIds.join(',')
-    this.setState({artistsIds, searchOptions: opts})
-  }
+	public componentDidMount() {
+		if (this.state.initiated) {
+			this.getGenres()
+		}
+	}
+
+	public getArtistIds = async (artistNames: string[]) => {
+		// tslint:disable-next-line:no-console
+		console.log('getting artists', artistNames)
+		const artists = (await this.spotify.searchArtists(artistNames))
+		// tslint:disable-next-line:no-console
+		console.log('artists from spot', artists)
+		const artistsIds = artists.filter(a => !!a).map(r => r.id);
+		const opts = this.state.searchOptions;
+		opts.seed_artists = artistsIds.join(',')
+		this.setState({ artistsIds, searchOptions: opts })
+	}
 
 
-  public getGenres = async () => {
-    let genres;
-    try {
-      genres = await this.spotify.getGenres();
-    } catch(e) {
-      // tslint:disable-next-line:no-console
-      console.log('e', e)
-      this.onError(e);
-    }
-    this.setState({genres})
-  }
-  public saveGenre = (genre: string) => {
-    const options = this.state.searchOptions;
-    options.seed_genres = genre;
-    this.setState({searchOptions: options})
-  }
-  public getRecommendations = async () => {
-    try {
-      const recommendations = await this.spotify.getRecommendations(this.state.searchOptions);
-      this.setState({ recommendations })
-    } catch (e) {
-      // tslint:disable-next-line:no-console
-      console.log('e', e)
-      this.onError(e);
-    }
-  }
+	public getGenres = async () => {
+		let genres;
+		try {
+			genres = await this.spotify.getGenres();
+		} catch (e) {
+			// tslint:disable-next-line:no-console
+			console.log('e', e)
+			this.onError(e);
+		}
+		this.setState({ genres })
+	}
+	public saveGenre = (genre: string) => {
+		const options = this.state.searchOptions;
+		options.seed_genres = genre;
+		this.setState({ searchOptions: options })
+	}
+	public getRecommendations = async () => {
+		try {
+			const recommendations = await this.spotify.getRecommendations(this.state.searchOptions);
+			this.setState({ recommendations })
+		} catch (e) {
+			// tslint:disable-next-line:no-console
+			console.log('e', e)
+			this.onError(e);
+		}
+	}
 
-  public onError = (e: any) => {
-    if (e.status === 401) {
-      this.setState({token: undefined, initiated: false })
-    }
-  }
+	public onError = (e: any) => {
+		if (e.status === 401) {
+			this.setState({ token: undefined, initiated: false })
+		}
+	}
 
-  public attributesOnChange = (attribute: IQueryAttribute) => {
-    // get current search options
-    const options = this.state.searchOptions;
-    // update this attribute with new value
-    if (!attribute.value) {
-      delete options[attribute.name];
-    } else {
-      options[attribute.name] = attribute.value;
-    }
-    this.setState({searchOptions: options})
-  }
+	public attributesOnChange = (attribute: IQueryAttribute) => {
+		// get current search options
+		const options = this.state.searchOptions;
+		// update this attribute with new value
+		if (!attribute.value) {
+			delete options[attribute.name];
+		} else {
+			options[attribute.name] = attribute.value;
+		}
+		this.setState({ searchOptions: options })
+	}
 
-  public validateSearch = () => {
-    const options = this.state.searchOptions;
-    const hasGenre = options.seed_genres ? true : false;
-    const hasArtist = options.seed_artists ? true : false;
-    return hasGenre || hasArtist;
-  }
+	public validateSearch = () => {
+		const options = this.state.searchOptions;
+		const hasGenre = options.seed_genres ? true : false;
+		const hasArtist = options.seed_artists ? true : false;
+		return hasGenre || hasArtist;
+	}
 
-  public showSearch = () => {
-    return (
-      <div className='grid'>
-        <ArtistSearch visible={this.state.initiated} onSearch={this.getArtistIds} onError={this.onError}/>
-        <GenreSearch visible={this.state.initiated} onSearch={this.saveGenre} genreList={this.state.genres as string []} onError={this.onError}/>
-        <QueryAttributes onChange={this.attributesOnChange}/>
-        <div>
-          <button onClick={this.getRecommendations} disabled={!this.validateSearch()}>recommend!</button>
-        </div>
-      </div>
-    )
-  }
+	public showSearch = () => {
+		return (
+			<div className='grid'>
+				<ArtistSearch visible={this.state.initiated} onSearch={this.getArtistIds} onError={this.onError} />
+				<GenreSearch visible={this.state.initiated} onSearch={this.saveGenre} genreList={this.state.genres as string[]} onError={this.onError} />
+				<QueryAttributes onChange={this.attributesOnChange} />
+				<div>
+					<button onClick={this.getRecommendations} disabled={!this.validateSearch()}>recommend!</button>
+				</div>
+			</div>
+		)
+	}
 
-  public showRecommendationResults = () => {
-    const recs = this.state.recommendations as SpotifyApi.RecommendationsFromSeedsResponse;
-    return (
-      <div>
-        <ul>
-          {recs.tracks.map(t => (<p key={t.id}><a href={t.external_urls.spotify}> 🎵 Song: {t.name} by: {t.artists.map(a => a.name).join(' and ')}</a></p>))}
-        </ul>
-      </div>
-    )
-  }
+	public showRecommendationResults = () => {
+		const recs = this.state.recommendations as SpotifyApi.RecommendationsFromSeedsResponse;
+		return (
+			<div>
+				<ul>
+					{recs.tracks.map(t => (<p key={t.id}><a href={t.external_urls.spotify}> 🎵 Song: {t.name} by: {t.artists.map(a => a.name).join(' and ')}</a></p>))}
+				</ul>
+			</div>
+		)
+	}
 
-  public render() {
-    return (
-      <div className="App">
-        {!this.state.initiated && <a href={this.loginUrl}>Log in to Spotify</a>}
-        {this.state.initiated && this.showSearch()}
-        {this.state.recommendations && this.showRecommendationResults()}
-        
-      </div>
-    );
-  }
+	public render() {
+		return (
+			<div className="App">
+				{!this.state.initiated && <a href={this.loginUrl}>Log in to Spotify</a>}
+				{this.state.initiated && this.showSearch()}
+				{this.state.recommendations && this.showRecommendationResults()}
+
+			</div>
+		);
+	}
 }
 
 export default Home;
