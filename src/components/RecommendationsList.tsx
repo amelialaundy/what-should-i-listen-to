c3245103169bfba02307f829cfc087b8055d939e
Amelia Laundy <amelialaundy@gmail.com>
2018-08-20 15:18:59 +1200

    allow playlist to have  a custom name

--- src/components/RecommendationsList.tsx
+++ src/components/RecommendationsList.tsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@material-ui/core";
 import * as React from "react";
 import { Spotify } from "../helpers/Spotify";
 
@@ -7,16 +8,17 @@ interface IProps {
 }
 interface IState {
 	playlistLink: string;
+	playlistName: string;
 }
 
 class RecommendationsList extends React.Component <IProps, IState> {
 
 	constructor(props: IProps) {
 		super(props);
-		this.state = {playlistLink: ''};
+		this.state = {playlistLink: '', playlistName: ''};
 	}
 	public createPlaylist = async () => {
-		const playlistName = 'test';
+		const {playlistName} = this.state;
 		const recs = this.props.recommendations as SpotifyApi.RecommendationsFromSeedsResponse;
 		const playListContents = recs.tracks.map(r => r.uri);
 		const res = await this.props.spotify.call<Promise<SpotifyApi.CreatePlaylistResponse>,string, string[]>(this.props.spotify.createPlaylistAndAddTracks, playlistName, this.onError, playListContents, 'error creating playlist');
@@ -28,7 +30,25 @@ class RecommendationsList extends React.Component <IProps, IState> {
 		console.log(e)
 		throw e;
 	}
-	
+
+	public onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const playlistName = event.target.value;
+		this.setState({ playlistName });
+	}
+	public getButton = () => {
+		return (
+			<div className='playlist-block'>
+				<TextField
+					type="text" 
+					name="artistList"
+					value={this.state.playlistName }
+					onChange={this.onChange}
+					placeholder='Playlist name'/>
+				<Button variant="outlined" disabled={!this.state.playlistName} onClick={this.createPlaylist} className='create-playlist'>Create new playlist</Button>
+				{this.state.playlistLink && <Button variant="outlined"  className='open-playlist'><a href={this.state.playlistLink}>open your playlist!</a></Button>}
+			</div>
+		)
+	}
 	public showRecommendationResults = () => {
 		const {recommendations} = this.props;
 		if (!recommendations ) { return null; }
@@ -37,7 +57,7 @@ class RecommendationsList extends React.Component <IProps, IState> {
 				<div className='recommendations-block'>
 					<h2 className='result-item'>Results</h2>
 					{recommendations.tracks.length === 0 && <h3>No tracks found try using less search attributes</h3>}
-					{recommendations.tracks.length > 0 && <button onClick={this.createPlaylist} className='create-playlist'>Create new Spotify playlist</button>}
+					{recommendations.tracks.length > 0 && this.getButton()}
 					{recommendations.tracks.map(t => (<a className='result-item' key={t.id} href={t.uri}> 🎵 Song: {t.name} by: {t.artists.map(a => a.name).join(' and ')}</a>))}
 					{this.state.playlistLink && <a href={this.state.playlistLink}>open your playlist!</a>}
 				</div>
