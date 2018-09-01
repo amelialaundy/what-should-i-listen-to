48dad88ab57ec808616543d4627338bf7dd668c2
Amelia Laundy <amelialaundy@gmail.com>
2018-09-02 10:59:44 +1200

    log vars

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,13 +3,16 @@ import { Route, Switch } from 'react-router-dom'
 import Callback from './Callback';
 import Home from './Home'
 
-const Main = () => (
+const Main = () => {
+	// tslint:disable-next-line:no-console
+	console.log('process.env.REACT_APP_URI', process.env.REACT_APP_URI)
+	return (
 	<main>
 		<Switch>
 			<Route exact={true} path={`${process.env.REACT_APP_URI}callback`} component={Callback}/>
 			<Route path={process.env.REACT_APP_URI} component={Home}/>
 		</Switch>
 	</main>
-)
+)}
 
 export default Main
