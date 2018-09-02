11f026c4fd8d8121feee18eb340a342c0da2a62b
Amelia Laundy <amelialaundy@gmail.com>
2018-09-02 15:43:26 +1200

    app_version in circle config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 
 class Header extends React.Component {
 		public render() {
+			// tslint:disable-next-line:no-console
+			console.log('process.env', process.env)
+
 			return (
 				<header className="App-header">
 				<h1 className="App-title">What should I listen to?</h1>
-				<p> v: {process.env.REACT_APP_VERSION}</p>
+				<p> v: {process.env.APP_VERSION}</p>
+				<p> v: {process.env.npm_package_version}</p>
 			</header>)
 		}
 	}
