90f928951e89d426d8c38c9b54287617962c3af6
Amelia Laundy <amelialaundy@gmail.com>
2018-09-02 14:59:32 +1200

    use appversion to version app and also display version on site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react'
+import * as React from 'react';
 
 class Header extends React.Component {
 		public render() {
 			return (
 				<header className="App-header">
 				<h1 className="App-title">What should I listen to?</h1>
+				<p> v: {process.env.REACT_APP_VERSION}</p>
 			</header>)
 		}
 	}
