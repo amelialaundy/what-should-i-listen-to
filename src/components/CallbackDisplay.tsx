9620e11ed68a21985dae0ff400ab090ee714d6d0
Amelia Laundy <amelialaundy@gmail.com>
2018-10-24 17:32:35 +1300

    use location.search

--- src/components/CallbackDisplay.tsx
+++ src/components/CallbackDisplay.tsx
@@ -5,7 +5,9 @@ import { RouteComponentProps } from 'react-router-dom'
 
 class CallbackDisplay extends React.Component<RouteComponentProps<any>, any> {
 	public render() {
-		const q = this.props.location.hash;
+		const q = this.props.location.search;
+		// tslint:disable-next-line:no-console
+		console.log(q)
 		// remove the `#` at start
 		const parsed = qs.parse(q.slice(1, q.length));
 		return (<p>code: {parsed.code}</p>)
