0b6932d143b8cc3bcb5b0418cfa99b60d6178f78
Amelia Laundy <amelialaundy@gmail.com>
2018-10-24 17:47:28 +1300

    copy token to clipboard

--- src/components/CallbackDisplay.tsx
+++ src/components/CallbackDisplay.tsx
@@ -4,13 +4,42 @@ import { RouteComponentProps } from 'react-router-dom'
 
 
 class CallbackDisplay extends React.Component<RouteComponentProps<any>, any> {
+	public code: any;
+	public textArea: HTMLTextAreaElement | null;
+	constructor(props: RouteComponentProps<any>) {
+		super(props);
+
+		this.state = { copySuccess: '' }
+	}
+	
+	public copyToClipboard = () => {
+		if (!this.textArea) { return; }
+		this.textArea.select();
+		document.execCommand('copy');
+		// This is just personal preference.
+		// I prefer to not show the the whole text area selected.
+		this.textArea.focus();
+		this.setState({ copySuccess: 'Copied!' });
+	};
+	
 	public render() {
 		const q = this.props.location.search;
 		// tslint:disable-next-line:no-console
 		console.log(q)
 		// remove the `#` at start
-		const parsed = qs.parse(q.slice(1, q.length));
-		return (<p>code: {parsed.code}</p>)
+		this.code = qs.parse(q.slice(1, q.length)).code;
+		return (
+			<div>
+				<form>
+					<textarea
+						ref={(textarea) => this.textArea = textarea}
+						defaultValue={this.code}
+					/>
+				</form>
+				<button onClick={this.copyToClipboard}>Copy</button> 
+				{this.state.copySuccess}
+		</div>
+		)
 	}
 }
 
